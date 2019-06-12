ae147aeded0b885d81b89f4a2db5cea387d9e3ba
László Bálint <laszlobalint1987@gmail.com>
2019-06-12 13:52:47 +0200

    Tiny change in controller

--- src/main/java/transporter/controllers/TransportController.java
+++ src/main/java/transporter/controllers/TransportController.java
@@ -1,6 +1,5 @@
 package transporter.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
