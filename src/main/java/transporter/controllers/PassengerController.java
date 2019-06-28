dd4e85416254e619acd3eec88f0db96272d9bf63
László Bálint <laszlobalint1987@gmail.com>
2019-06-28 14:27:37 +0200

    Creating and resolving JWT

--- src/main/java/transporter/controllers/PassengerController.java
+++ src/main/java/transporter/controllers/PassengerController.java
@@ -1,12 +1,21 @@
 package transporter.controllers;
 
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import transporter.entities.Passenger;
 import transporter.services.PassengerService;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/passenger")
@@ -14,6 +23,9 @@ import java.util.List;
 public class PassengerController {
 
     private PassengerService passengerService;
+
+    @Autowired
+    private Environment environment;
 
     public PassengerController(PassengerService passengerService) {
         this.passengerService = passengerService;
@@ -31,8 +43,7 @@ public class PassengerController {
     }
 
     @GetMapping
-    public Passenger listPassenger() {
-        Long id = null;
+    public Passenger listPassenger(Long id) {
         return passengerService.listPassenger(id);
     }
 
@@ -54,4 +65,70 @@ public class PassengerController {
         model.addAttribute("message", "Sikeresen törölted a foglalásodat!");
         return model;
     }
-}
+
+
+    // TOKEN FUNCTIONALITY - UNDER IMPLEMENTATION
+    @GetMapping("/token")
+    public boolean getToken() {
+        String token = createJWT(listPassenger(1L));
+        // return "ID of the user: " + decodeJWT(token).getSubject();
+        return validateToken(token);
+    }
+
+    private String createJWT(Passenger passenger) {
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        long ttlMillis = 604800000L;
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(environment.getProperty("secretKey"));
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        JwtBuilder builder = Jwts.builder().setId(UUID.randomUUID().toString())
+                .setIssuedAt(now)
+                .setSubject(passenger.getId().toString())
+                .setIssuer(passenger.getName())
+                .signWith(signatureAlgorithm, signingKey);
+
+        long expMillis = nowMillis + ttlMillis;
+        Date exp = new Date(expMillis);
+        builder.setExpiration(exp);
+
+        return builder.compact();
+    }
+
+    private Claims decodeJWT(String jwt) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(environment.getProperty("secretKey")))
+                .parseClaimsJws(jwt).getBody();
+        System.out.println("Token ID: " + claims.getId());
+        System.out.println("User ID: " + claims.getSubject());
+        System.out.println("Name: " + claims.getIssuer());
+        System.out.println("Expiration: " + claims.getExpiration());
+
+        return claims;
+    }
+
+    private String getPassengerName(String token) {
+
+        return Jwts.parser().setSigningKey(environment.getProperty("secretKey")).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    private String resolveToken(HttpServletRequest req) {
+
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
+    }
+
+    private boolean validateToken(String token) {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(environment.getProperty("secretKey")).parseClaimsJws(token);
+
+            return !claims.getBody().getExpiration().before(new Date());
+        }
+    }
